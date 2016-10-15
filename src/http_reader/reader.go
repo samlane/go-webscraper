5efc81443d2e086a92c5f2df08b0903172e9c092
Sam Lane <demoncoder38@gmail.com>
2016-10-14 21:35:36 -0500

    Adding http_reader

---
+++ src/http_reader/reader.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://theguillotine.com/open-tournament-calendar/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", body)
+}
